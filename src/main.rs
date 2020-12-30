2b658fdd61be1fc744ff4035020f15f12e5a3e57
杨国平 <756517553@qq.com>
2020-12-30 23:35:35 +0800

    add read file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-
+// use std::env;
+use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    // let args: Vec<String> = env::args().collect();
+    // let file = args.get(0);
+    let res = fs::read_to_string("/Users/ouyuan/Desktop/workdir/kongfu/src/test.k").unwrap();
+    println!("size ={}",res);
 }
